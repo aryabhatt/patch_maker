e15598538450678157cf61da0936091e710e4ea4
aryabhatt <dinesharma@gmail.com>
2025-09-25 10:30:50 -0700

    Add maxWidth() and maxHeight() accessors and member variables to MainWindow

--- src/main_window.h
+++ src/main_window.h
@@ -9,6 +9,8 @@ class MainWindow : public QMainWindow {
     Q_OBJECT
   public:
     MainWindow(QWidget *parent = nullptr);
+    int maxWidth() const { return maxW; }
+    int maxHeight() const { return maxH; }
 
   private slots:
     void openFile();
@@ -16,6 +18,8 @@ class MainWindow : public QMainWindow {
 
   private:
     ImageViewer *viewer;
+    int maxW;
+    int maxH;
 };
 
 #endif // MAIN_WINDOW__H
