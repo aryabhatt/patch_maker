2bc89c4bb3b5ef5bb208950466665b0b84825489
Dinesh Kumar <dkumar@lbl.gov>
2025-11-19 10:35:06 -0800

    minor change

--- src/main.cpp
+++ src/main.cpp
@@ -4,10 +4,9 @@
 #include "io/array.h"
 #include "main_window.h"
 
-int main(int argc, char *argv[]) {
+int main(int argc, char **argv) {
     QApplication app(argc, argv);
 
-    // Example: Create a 3D float array: depth x height x width
     MainWindow win;
     win.resize(512, 512);
     win.show();
