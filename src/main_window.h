78822f503f92728c62f51643aeeff6f3e7aa8166
aryabhatt <dinesharma@gmail.com>
2026-01-17 11:32:02 -0800

    Add patch export and pick actions to MainWindow interface
    - Added export_patches, onPicksCompleted, and onPickUpdated slots
    - Added std::filesystem::path member for subdir name
    - Added QAction members for export and pick/reset actions

--- src/main_window.h
+++ src/main_window.h
@@ -2,11 +2,13 @@
 #ifndef MAIN_WINDOW__H
 #define MAIN_WINDOW__H
 #include <QMainWindow>
+#include <filesystem>
 
 #include "image_viewer.h"
 
 class MainWindow : public QMainWindow {
     Q_OBJECT
+
   public:
     MainWindow(QWidget *parent = nullptr);
     int maxWidth() const { return maxW; }
@@ -14,10 +16,17 @@ class MainWindow : public QMainWindow {
 
   private slots:
     void openFile();
-    void toggle_save(bool);
+    void export_patches();
+    void onPicksCompleted(QPoint, QPoint);
+    void onPickUpdated(int, QPoint);
 
   private:
+    std::filesystem::path subdir_name;
     ImageViewer *viewer;
+    QAction *exportAction;
+    QAction *pick1Action;
+    QAction *pick2Action;
+    QAction *resetAction;
     int maxW;
     int maxH;
 };
