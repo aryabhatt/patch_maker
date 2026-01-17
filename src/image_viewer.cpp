33bf4cce88058df995db26f8f4285772de8f2d87
aryabhatt <dinesharma@gmail.com>
2026-01-17 11:31:19 -0800

    Add patch export functionality and ROI picking logic
    - Added `export_patches()` to save random patches from images.
    - Implemented center/radius picking via mouse events.
    - Updated image scaling logic for proper coordinate mapping.
    - Removed old `saveROI()` method.

--- src/image_viewer.cpp
+++ src/image_viewer.cpp
@@ -2,18 +2,25 @@
 #include <QMouseEvent>
 #include <algorithm>
 #include <cstdint>
+#include <cstdlib>
 #include <qevent.h>
 #include <qgraphicsview.h>
 #include <qnamespace.h>
-#include <string>
 #include <unistd.h>
 
 #include "image_viewer.h"
 #include "io/tiff/tiffio.h"
 #include "main_window.h"
+#include "save_patch.h"
+
+constexpr int PATCHES_PER_FRAME = 1;
+
+template <typename T> T random() { return static_cast<T>(rand()) / static_cast<T>(RAND_MAX); }
 
 ImageViewer::ImageViewer(const tomocam::Array<float> &images, QWidget *parent)
-    : QGraphicsView(parent), imageStack(images), currentIndex(0), counter(0), save_roi_flag(false) {
+    : QGraphicsView(parent), imageStack(images), currentIndex(0), counter(0), save_roi_flag(false),
+      pickedCenter(false), pickedRadius(false), pickMode(PickMode::None), scaleH(1.f), scaleW(1.f) {
+
     scene = new QGraphicsScene(this);
     setScene(scene);
     setDragMode(QGraphicsView::ScrollHandDrag);
@@ -50,7 +57,6 @@ QImage ImageViewer::floatArrayToQImage(const tomocam::Slice<float> &array) {
             line[x] = static_cast<uchar>(norm * 255.0f);
         }
     }
-
     // resize of image is too big
     // get main window
     MainWindow *mainWin;
@@ -65,6 +71,8 @@ QImage ImageViewer::floatArrayToQImage(const tomocam::Slice<float> &array) {
         if (h > mainWin->maxHeight() || w > mainWin->maxWidth()) {
             img = img.scaled(mainWin->maxWidth(), mainWin->maxHeight(),
                              Qt::KeepAspectRatioByExpanding);
+            scaleH = static_cast<float>(h) / static_cast<float>(mainWin->maxHeight());
+            scaleW = static_cast<float>(w) / static_cast<float>(mainWin->maxWidth());
         }
     }
     return img;
@@ -87,13 +95,27 @@ void ImageViewer::wheelEvent(QWheelEvent *event) {
 }
 
 void ImageViewer::mousePressEvent(QMouseEvent *event) {
+
     QPointF scenePos = mapToScene(event->pos());
     int x = static_cast<int>(scenePos.x());
     int y = static_cast<int>(scenePos.y());
 
-    if (x >= 0 && y >= 0 && y < static_cast<int>(imageStack.nrows()) &&
+    if (imageStack.size() > 0 && x >= 0 && y >= 0 && y < static_cast<int>(imageStack.nrows()) &&
         x < static_cast<int>(imageStack.ncols())) {
-        saveROI(currentIndex, y, x);
+        if (pickMode == PickMode::PickP1) {
+            center = QPoint(x, y);
+            pickedCenter = true;
+            emit pickUpdated(1, center);
+            pickMode = PickMode::None;
+        } else if (pickMode == PickMode::PickP2) {
+            radius = QPoint(x, y);
+            pickedRadius = true;
+            emit pickUpdated(2, radius);
+            pickMode = PickMode::None;
+        }
+        if (pickedCenter && pickedRadius) {
+            emit picksCompleted(center, radius);
+        }
     }
     QGraphicsView::mousePressEvent(event);
 }
@@ -102,34 +124,6 @@ void ImageViewer::updateImageStack(const tomocam::Array<float> &arr) {
     imageStack = arr;
     currentIndex = 0;
     updateImage();
-}
-
-void ImageViewer::saveROI(int z, int y, int x) {
-
-    if (!save_roi_flag)
-        return;
-
-    uint32_t h = 256;
-    uint32_t w = 256;
-    if (y - h / 2 < 0 || y + h / 2 >= imageStack.nrows() || x - w / 2 < 0 ||
-        x + w / 2 >= imageStack.ncols()) {
-        return;
-    }
-    tomocam::Array<uint32_t> box(1, h, w);
-
-    uint32_t slc = (uint32_t)z;
-    uint32_t ibeg = y - h / 2;
-    uint32_t jbeg = x - h / 2;
-    for (uint32_t i = 0; i < h; ++i) {
-        for (uint32_t j = 0; j < w; ++j) {
-            float pxlval = imageStack[{slc, ibeg + i, jbeg + j}];
-            box[{0, i, j}] = static_cast<uint32_t>(255.0 * (pxlval - minVal) / (maxVal - minVal));
-        }
-    }
-    char path[20];
-    snprintf(path, 20, "patch%05d.tif", counter);
-    tomocam::tiff::write<uint32_t>(std::string(path), box);
-    counter += 1;
 }
 
 void ImageViewer::keyPressEvent(QKeyEvent *event) {
@@ -167,5 +161,28 @@ void ImageViewer::keyPressEvent(QKeyEvent *event) {
 
     if (currentIndex != oldIndex) {
         updateImage();
+    }
+}
+
+void ImageViewer::export_patches(std::filesystem::path subdir) {
+
+    float maxR = get_realRadius();
+    float cenX = get_realCenX();
+    float cenY = get_realCenY();
+
+    float constexpr SECTOR_SIZE = 2 * M_PI / PATCHES_PER_FRAME;
+    for (int i = 0; i < imageStack.nslices(); i++) {
+        for (int j = 0; j < PATCHES_PER_FRAME; j++) {
+            float t = (j + random<float>()) * SECTOR_SIZE;
+            float r = random<float>() * maxR;
+            float x = cenX + r * std::cos(t);
+            float y = cenY + r * std::sin(t);
+            char pname[20];
+            snprintf(pname, 20, "%05d.tif", counter);
+            auto tifname = std::filesystem::path(pname);
+            tomocam::dims_t loc{(uint32_t)i, (uint32_t)y, (uint32_t)x};
+            save_patch((subdir / tifname).string(), imageStack, loc);
+            counter += 1;
+        }
     }
 }
