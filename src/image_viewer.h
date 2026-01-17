cccd978a4274c021240ee9369c1aaf0e3572a0c6
aryabhatt <dinesharma@gmail.com>
2026-01-17 11:31:08 -0800

    Add pixel picking functionality to ImageViewer
    - Introduced `PickMode` enum for selecting pick modes.
    - Added methods and members to support picking center and radius points.
    - Implemented accessors for picked points and their real coordinates.
    - Added signals for pick updates and completion.
    - Added `export_patches` method for exporting selected patches.
    - Refactored ROI save toggle and related member variables.

--- src/image_viewer.h
+++ src/image_viewer.h
@@ -3,12 +3,15 @@
 #include <QGraphicsView>
 #include <QImage>
 #include <QWheelEvent>
+#include <filesystem>
 #include <qevent.h>
 
 #include "io/array.h"
 
 #ifndef IMG_VIEWER__H
 #define IMG_VIEWER__H
+
+enum class PickMode { None, PickP1, PickP2 };
 
 class ImageViewer : public QGraphicsView {
     Q_OBJECT
@@ -17,11 +20,33 @@ class ImageViewer : public QGraphicsView {
     ImageViewer(const tomocam::Array<float> &, QWidget *parent = nullptr);
     void updateImage();
     void updateImageStack(const tomocam::Array<float> &);
-    void saveROI(int, int, int);
+    void export_patches(std::filesystem::path);
+
+    // Access picked pixels
+    void setPickMode(PickMode mode) { pickMode = mode; }
+    QPoint getCenter() const { return center; }
+    QPoint getRadius() const { return radius; }
+    bool picksReady() const { return pickedCenter && pickedRadius; }
+    float get_realCenX() const { return scaleW * center.x(); }
+    float get_realCenY() const { return scaleH * center.y(); }
+    float get_realRadius() const {
+        return std::sqrt(std::pow(scaleW * (radius.x() - center.x()), 2) +
+                         std::pow(scaleH * (radius.y() - center.y()), 2));
+    }
+    // reset center + radius
+    void reset() {
+        pickedCenter = false;
+        pickedRadius = false;
+    }
+
+  signals:
+    void picksCompleted(QPoint p1, QPoint p2);
+    void pickUpdated(int, QPoint);
 
   public slots:
     void toggle_save_option(bool sw) {
-        if (sw) save_roi_flag = true;
+        if (sw)
+            save_roi_flag = true;
         else
             save_roi_flag = false;
     }
@@ -39,6 +64,16 @@ class ImageViewer : public QGraphicsView {
     bool save_roi_flag;
     float minVal;
     float maxVal;
+    QPoint center;
+    QPoint radius;
+    bool pickedCenter;
+    bool pickedRadius;
+    PickMode pickMode;
+    float scaleW;
+    float scaleH;
+    float realCenX;
+    float realCenY;
+    float realRmax;
 
     QImage floatArrayToQImage(const tomocam::Slice<float> &);
 };
